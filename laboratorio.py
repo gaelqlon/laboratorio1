58a70f2f2ece3d217a9a572e698ee157f516c125
gael alexander cardona lima <cardonagael11@gmailcom>
2023-03-29 09:04:52 -0600

    se le pide al usuario ingresas los datos para la operacion

---
+++ laboratorio.py
@@ -0,0 +1,8 @@
+print("Ingrese 2 numeros para poder interactuar con ellos :")
+n1 = int(input("ingrese el primer numero: "))
+n2 = int(input("ingrese el segundo  numero: "))
+print(f" La suma de los dos numeros es:  {n1+n2}")
+print(f" La sustracion de los numeros es de :  {n1-n2}")
+print(f" la multiplicacion de los dos numeros es de :  {n1*n2}")
+print(f" La Division de los dos numeros es de :  {n1/n2}"
+
