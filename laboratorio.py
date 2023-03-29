32fb46772b337aed0ac79b66f0c8dfd08786a345
gael alexander cardona lima <cardonagael11@gmailcom>
2023-03-29 09:06:35 -0600

    se arreglo un problema con las operaciones

--- laboratorio.py
+++ laboratorio.py
@@ -1,8 +1,9 @@
-print("Ingrese 2 numeros para poder interactuar con ellos :")
+print("Ingrese los dos numeros para poder realizar las correspondientes ejercicios:")
 n1 = int(input("ingrese el primer numero: "))
 n2 = int(input("ingrese el segundo  numero: "))
 print(f" La suma de los dos numeros es:  {n1+n2}")
 print(f" La sustracion de los numeros es de :  {n1-n2}")
 print(f" la multiplicacion de los dos numeros es de :  {n1*n2}")
-print(f" La Division de los dos numeros es de :  {n1/n2}"
+
+print(f" La Division de los dos numeros es de :  {n1/n2}")
 
